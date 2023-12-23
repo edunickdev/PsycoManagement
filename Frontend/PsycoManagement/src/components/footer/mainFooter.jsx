ed8a8f39c4950884a512ca741f3e843bda552d29
El Código Recuerda <114629663+edunickdev@users.noreply.github.com>
2023-12-23 12:50:36 -0500

    vista home, completa
    queda pendiente redirección a registro una vez este construida esa vista

--- Frontend/PsycoManagement/src/components/footer/mainFooter.jsx
+++ Frontend/PsycoManagement/src/components/footer/mainFooter.jsx
@@ -1,7 +1,7 @@
 const Footer = () => {
     return (
         <footer className="col-span-12 flex justify-center items-center h-16 bg-blue-900 fixed bottom-0 left-0 right-0">
-            <span className="text-center font-semibold text-gray-400">Página desarrollada y administrada por <a href="#" className="text-blue-400"> Nicolás Sarmienton (SnowDev) </a></span>
+            <span className="text-center font-semibold text-gray-400">Página desarrollada y administrada por <a href="#" className="text-blue-400"> Nicolás Sarmiento (SnowDev) </a></span>
         </footer>
     )
 };
