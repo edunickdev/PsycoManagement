4fab49ba1a7f1a28aa2811abf7cec49c07386a09
El Código Recuerda <114629663+edunickdev@users.noreply.github.com>
2023-12-27 17:56:33 -0500

    proceso de encriptacion en registro para el password en front y back ok
    queda pendiente el proceso de inicio de sesion para terminar lo que seria el proceso de autenticación como tal.

--- Frontend/PsycoManagement/src/components/register/contentSignUp.jsx
+++ Frontend/PsycoManagement/src/components/register/contentSignUp.jsx
@@ -1,9 +1,21 @@
 import { Button, Input } from "@nextui-org/react";
 import { useState } from "react";
 import { useForm, Controller } from "react-hook-form";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import bcrypt from "bcryptjs";
 
 const ContentSignUp = () => {
+  const notify = ({ text, type }) => {
+    toast[type](text, {
+      position: "top-right",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      draggable: true,
+    });
+  };
+
   const[mode, setMode] = useState(false);
   const { control, handleSubmit, formState: { errors }, reset } = useForm();
 
@@ -29,10 +41,11 @@ const ContentSignUp = () => {
     const responseObject = await response.json();
 
     if (responseObject.registered === 'usuario creado') {
-      alert('Usuario registrado correctamente');
+      notify({ text: responseObject.message, type: "success" })
+      setMode(!mode);
       reset();
     } else {
-      alert(responseObject.message);
+      notify({ text: responseObject.message, type: "error" })
     }
   };
 
@@ -89,55 +102,56 @@ const ContentSignUp = () => {
   ];
 
   return (
-    <div className="absolute inset-0 flex items-center justify-center z-10">
-      <div className="relative flex justify-center">
-        <div className="flex flex-col justify-center items-center bg-slate-200 h-auto py-6 px-10 rounded-xl -mb-16 transition-height duration-300">
-          <h2 className="text-3xl mb-3">{mode === false ? "Regístrate ahora" : "Inicio de sesión"}</h2>
-          <form
-            onSubmit={handleSubmit(onSubmit)}
-            className="flex flex-col justify-center"
-          >
-            {fields.map((input) => {
-              return (
-                <Controller
-                  type={input.type}
-                  key={input.name}
-                  rules={input.validations}
-                  name={input.name}
-                  control={control}
-                  defaultValue=""
-                  render={({ field }) => (
-                    <Input
-                      type={input.type}
-                      labelPlacement="inside"
-                      label={field.name}
-                      {...field}
-                      className={`py-[2px] w-60 ${mode === true && input.name === "Nombre" || mode === true && input.name == "Apellido" ? "hidden" : ""}`}
-                      size="sm"
-                      errorMessage={errors[input.name]?.message}
-                    />
-                  )}
-                />
-              );
-            })}
-            <span className="text-tiny self-end px-2">
-              {mode === false ? "¿Ya estas registrado?" : "¿No tienes una cuenta?"}
-              <a href="#" className="underline text-blue-700 font-semibold px-1" onClick={()=> setMode(!mode)}>
-                {mode === false ? "Inicia Sesión" : "Regístrate"}
-              </a>
-            </span>
-            <Button
-              type="submit"
-              variant="solid"
-              className="mt-3 font-semibold"
-              color="success"
+      <div className="absolute inset-0 flex items-center justify-center z-10">
+        <div className="relative flex justify-center">
+          <div className="flex flex-col justify-center items-center bg-slate-200 h-auto py-6 px-10 rounded-xl -mb-16 transition-height duration-300">
+            <h2 className="text-3xl mb-3">{mode === false ? "Regístrate ahora" : "Inicio de sesión"}</h2>
+            <form
+              onSubmit={handleSubmit(onSubmit)}
+              className="flex flex-col justify-center"
             >
-              { mode === false ? "Registrarse" : "Iniciar Sesión"}
-            </Button>
-          </form>
+              {fields.map((input) => {
+                return (
+                  <Controller
+                    type={input.type}
+                    key={input.name}
+                    rules={input.validations}
+                    name={input.name}
+                    control={control}
+                    defaultValue=""
+                    render={({ field }) => (
+                      <Input
+                        type={input.type}
+                        labelPlacement="inside"
+                        label={field.name}
+                        {...field}
+                        className={`py-[2px] w-60 ${mode === true && input.name === "Nombre" || mode === true && input.name == "Apellido" ? "hidden" : ""}`}
+                        size="sm"
+                        errorMessage={errors[input.name]?.message}
+                      />
+                    )}
+                  />
+                );
+              })}
+              <span className="text-tiny self-end px-2">
+                {mode === false ? "¿Ya estas registrado?" : "¿No tienes una cuenta?"}
+                <a href="#" className="underline text-blue-700 font-semibold px-1" onClick={()=> setMode(!mode)}>
+                  {mode === false ? "Inicia Sesión" : "Regístrate"}
+                </a>
+              </span>
+              <Button
+                type="submit"
+                variant="solid"
+                className="mt-3 font-semibold"
+                color="success"
+              >
+                {mode === false ? "Registrarse" : "Iniciar Sesión"}
+              </Button>
+              <ToastContainer className={"mt-12"} />
+            </form>
+          </div>
         </div>
       </div>
-    </div>
   );
 };
 
