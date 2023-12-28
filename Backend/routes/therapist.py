de7ac227025e57f539cc19f17b2d1034decbe089
El Código Recuerda <114629663+edunickdev@users.noreply.github.com>
2023-12-27 23:53:13 -0500

    login implementado en un 80%, ya es funcional

--- Backend/routes/therapist.py
+++ Backend/routes/therapist.py
@@ -1,12 +1,11 @@
-from fastapi import APIRouter
+import bcrypt
+from fastapi import APIRouter, Depends, Form, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
 from jwt import encode as jwt_encode
 from config.connection import secret_key
 from passlib.context import CryptContext
-
-
 from models.therapist_model import Therapist
 from config.connection import user_collection
-from schemas.therapist_schema import therapistEntityList
 
 therapist = APIRouter()
 
@@ -19,14 +18,11 @@ def generate_token(email: str) -> str:
 
 @therapist.post("/auth/sign-up", tags=["Authentication"])
 def register_therapist(therapist: Therapist):
-    print(f"lo que viene del front: {therapist.password}")
-    therapist.password = pwd_context.hash(therapist.password)
-    print(f"lo que queda en el backend al final: {therapist.password}")
     is_exist = user_collection.find_one({"email": therapist.email})
     if is_exist:
         return {
             "message":f"El correo {therapist.email} ya se encuentra registrado",
-            "registered": "usuario ya existe"
+            "status": "usuario ya existe"
             }
     new_therapist = dict(therapist)
     new_id = user_collection.insert_one(new_therapist).inserted_id
@@ -36,12 +32,33 @@ def register_therapist(therapist: Therapist):
             "id": new_therapist["_id"], 
             "token": new_therapist["token"],
             "message": "Usuario creado correctamente",
-            "registered": "usuario creado"
+            "status": "usuario creado"
         }
 
 
-@therapist.get("/auth/login", tags=["Authentication"])
-def login_therapist():
-    cursor = user_collection.find()
-    return therapistEntityList(cursor)
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+def verify_user(email: str, password: str):
+    user = user_collection.find_one({"email": email})
+    if user and bcrypt.checkpw(password.encode("utf-8"), user["password"].encode("utf-8")):
+        return user
+    raise HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Credenciales incorrectos",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+
+@therapist.post("/auth/login", tags=["Authentication"])
+async def login_therapist( therapist: Therapist ):
+    user = verify_user(therapist.email, therapist.password)
+    if user is None:
+        return {"message": "Credenciales incorrectos"}
+    new_token = generate_token(user["email"])
+    user["token"] = new_token
+    return {
+        "token": new_token,
+        "message": "Inicio de sesión exitoso",
+        "status": "inicio exitoso"
+        }
+    
 
