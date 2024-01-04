795b4afb0913ce2b5a613e953c153403210c8c8d
El Código Recuerda <114629663+edunickdev@users.noreply.github.com>
2024-01-04 18:26:53 -0500

    update

--- Frontend/PsycoManagement/src/components/consultant/sectionConsultans.jsx
+++ Frontend/PsycoManagement/src/components/consultant/sectionConsultans.jsx
@@ -1,17 +1,34 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable no-unused-vars */
+import { useContext, useEffect, useState } from "react";
 import ConsultantItem from "./consultantItem";
+import { AuthTherapist } from "../../context/AuthContext";
 
 
-const SectionConsultant = ({ consultants }) => {
+const SectionConsultant = () => {
+
+  const TherapistAuth = useContext(AuthTherapist);
+  
+  const [data, setData] = useState([]);
+
+  useEffect (() => {
+    const getData = () => {
+      fetch("http://127.0.0.1:8000/consultants?id_therapist=658da3e92d4804b8445e80eb")
+        .then((response) => response.json())
+        .then((data) => {
+          setData(data["consultants"]);
+        });
+      };
+    getData();
+  }, []);
 
   return (
     <div className="col-span-12 row-span-1 py-5 mt-10 mx-10">
       <div className="flex flex-wrap gap-3">
-        {consultants.map((consultant) => { 
-          {/* return ( */}
-            <ConsultantItem />
-          {/* ); */}
+        {data.map((consultant, index) => { 
+          return (
+            <ConsultantItem key={index} consultant={consultant} />
+          ); 
          })}
       </div>
     </div>
