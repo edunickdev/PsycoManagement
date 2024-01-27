5c6fbf1128b736299cfff53c5736d2ba0c82f4c2
El Código Recuerda <114629663+edunickdev@users.noreply.github.com>
2024-01-27 18:21:45 -0500

    update footer y ajuste espacio calendar

--- Frontend/PsycoManagement/src/components/calendar/Calendar.jsx
+++ Frontend/PsycoManagement/src/components/calendar/Calendar.jsx
@@ -1,5 +1,14 @@
-import { Button, Modal, ModalBody, ModalContent, ModalHeader, useDisclosure } from "@nextui-org/react";
-import "./calendar.css"
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/prop-types */
+import {
+  Button,
+  Modal,
+  ModalBody,
+  ModalContent,
+  ModalHeader,
+  useDisclosure,
+} from "@nextui-org/react";
+import "./calendar.css";
 import { Calendar, dayjsLocalizer } from "react-big-calendar";
 import "react-big-calendar/lib/css/react-big-calendar.css";
 import dayjs from "dayjs";
@@ -7,77 +16,71 @@ import "dayjs/locale/es";
 
 dayjs.locale("es");
 
-
 const CustomCalendar = () => {
+  const events = [
+    {
+      start: dayjs("2024-01-05T13:00:00").toDate(),
+      end: dayjs("2024-01-05T13:40:00").toDate(),
+      title: "event 1",
+    },
+    {
+      start: dayjs("2024-01-10T13:00:00").toDate(),
+      end: dayjs("2024-01-10T15:30:00").toDate(),
+      title: "event 2",
+    },
+    {
+      start: dayjs("2024-01-12T13:00:00").toDate(),
+      end: dayjs("2024-01-12T13:40:00").toDate(),
+      title: "event 3",
+    },
+    {
+      start: dayjs("2024-01-16T13:00:00").toDate(),
+      end: dayjs("2024-01-17T15:30:00").toDate(),
+      title: "event 4",
+    },
+  ];
 
-    const events = [
-        {
-            start: dayjs('2024-01-05T13:00:00').toDate(),
-            end: dayjs('2024-01-05T13:40:00').toDate(),
-            title: 'event 1'
-        },
-        {
-            start: dayjs('2024-01-10T13:00:00').toDate(),
-            end: dayjs('2024-01-10T15:30:00').toDate(),
-            title: 'event 2'
-        },
-        {
-            start: dayjs('2024-01-12T13:00:00').toDate(),
-            end: dayjs('2024-01-12T13:40:00').toDate(),
-            title: 'event 3'
-        },
-        {
-            start: dayjs('2024-01-16T13:00:00').toDate(),
-            end: dayjs('2024-01-17T15:30:00').toDate(),
-            title: 'event 4'
-        }
-    ]
+  const messages = {
+    allDay: "Todo el día",
+    previous: "<",
+    next: ">",
+    today: "Hoy",
+    month: "Mes",
+    week: "Semana",
+    day: "Día",
+    agenda: "Agenda",
+    date: "Fecha",
+    time: "Hora",
+    event: "Evento",
+    noEventsInRange: "Sin eventos",
+  };
 
-    const messages = {
-        allDay: "Todo el día",
-        previous: "<",
-        next: ">",
-        today: "Hoy",
-        month: "Mes",
-        week: "Semana",
-        day: "Día",
-        agenda: "Agenda",
-        date: "Fecha",
-        time: "Hora",
-        event: "Evento",
-        noEventsInRange: "Sin eventos"
-    };
+  const handleEventClick = (event, e) => {
+    alert("Event:" + event.title);
+  };
 
-    const handleEventClick = (event, e) => {
-        alert('Event:' + event.title);
-    }
+  const components = {
+    event: (props) => {
+      console.log(props);
+      return <div>{props.title}</div>;
+    },
+  };
 
-    const components = {
-        event: props => {
-            console.log(props);
-            return <div>{props.title}</div>
-        }
-    }
-
-    const localizer = dayjsLocalizer(dayjs)
-    return (
-        <div className="mt-24">
-            <Calendar
-                style={{
-                    width: '100%',
-                    height: '100vh'
-                }}
-                localizer={localizer}
-                //events={events}
-                views={["month", "week", "day"]}
-                defaultView={"month"}
-                messages={messages}
-                onSelectEvent={handleEventClick}
-                events={events}
-                components={components}
-            />
-        </div>
-    )
-}
+  const localizer = dayjsLocalizer(dayjs);
+  return (
+    <>
+      <Calendar
+        localizer={localizer}
+        views={["month", "week", "day"]}
+        defaultView={"month"}
+        messages={messages}
+        onSelectEvent={handleEventClick}
+        events={events}
+        components={components}
+        className="col-span-7 mt-24 w-full h-[450px]"
+      />
+    </>
+  );
+};
 
 export default CustomCalendar;
--- Frontend/PsycoManagement/src/pages/AgendaPage.jsx
+++ Frontend/PsycoManagement/src/pages/AgendaPage.jsx
@@ -1,17 +1,17 @@
-import React from "react";
 import Footer from "../components/footer/mainFooter";
 import CustomCalendar from "../components/calendar/Calendar";
+import Prueba from "../components/calendar/prueba";
+import Prueba2 from "../components/calendar/prueba2";
 
 const AgendaPage = () => {
-    return (
-        <div className="grid grid-cols-12">
-            <div className="cols-span-3">
-            </div>
-            <div className="cols-span-9">
-                <CustomCalendar/>
-            </div>         
-        </div>
-    );
+  return (
+    <div className="grid grid-cols-12">
+      <Prueba />
+      <CustomCalendar />
+      <Prueba2 />
+      <Footer />
+    </div>
+  );
 };
 
 export default AgendaPage;
